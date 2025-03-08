efc3c030f55466c08c561db1fed7d49a692f9274
ofekhalabi <Halabi1205@gmail.com>
2025-03-08 20:03:48 +0200

    check mongo replica set

--- polybot/test_bot.py
+++ polybot/test_bot.py
@@ -7,23 +7,20 @@ import boto3
 from botocore.exceptions import NoCredentialsError
 import requests
 
-try:
-    BUCKET_NAME = os.environ['BUCKET_NAME']
-    SQS_URL = os.environ['SQS_URL']
-except KeyError as e:
-    raise RuntimeError(f"Missing required environment variable: {e}")
-
 class Bot:
 
     def __init__(self, token, telegram_chat_url):
         # create a new instance of the TeleBot class.
         # all communication with Telegram servers are done using self.telegram_bot_client
         self.telegram_bot_client = telebot.TeleBot(token)
+
         # remove any existing webhooks configured in Telegram servers
         self.telegram_bot_client.remove_webhook()
         time.sleep(0.5)
+
         # set the webhook URL
-        self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/',certificate=POLYBOT_CERTIFICATE)
+        self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', timeout=60)
+
         logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
 
     def send_text(self, chat_id, text):
@@ -64,13 +61,24 @@ class Bot:
             InputFile(img_path)
         )
 
+    def handle_message(self, msg):
+        """Bot Main message handler"""
+        logger.info(f'Incoming message: {msg}')
+        if 'text' in msg:
+            self.send_text(msg['chat']['id'], f'Your original message: {msg["text"]}')
+        elif self.is_current_msg_photo(msg):
+            self.handle_photo_message(msg)
+        else:
+            self.send_text(msg['chat']['id'], "Unsupported message type")
+
     def handle_photo_message(self, msg):
         chat_id = msg['chat']['id']
         photo_path = self.download_user_photo(msg)
         # upload the image to S3 Bucket ofekh-polybotservicedocker-project
         s3 = boto3.client('s3')
-        bucket_name = BUCKET_NAME
+        bucket_name = os.getenv('BUCKET_NAME')
         s3_image_key_upload = f'{chat_id}_teleBOT_picture.jpg'
+
         try:
             # Upload predicted image back to S3
             s3.upload_file(str(photo_path), bucket_name, s3_image_key_upload)
@@ -84,47 +92,39 @@ class Bot:
         except Exception as e:
             logger.error(f"Error uploading file: {e}")
             return f"Error uploading file: {e}", 500
-        
-        # send an HTTP request to the `SQS` service for prediction
-        sqs_url = SQS_URL
+
+        # send an HTTP request to the `yolo5` service for prediction
+        url = "http://yolo5:8081/predict"
         params = {"imgName": s3_image_key_upload}
-        try:
-            response = requests.post(sqs_url, json=params)
-            logger.info(f"Response from SQS: {response.json()}")
-            self.send_text(msg['chat']['id'], f'Your image is being processed. Please wait...')
-        except Exception as e:
-            logger.error(f"Error sending message to SQS: {e}")
-            return f"Error sending message to SQS: {e}", 500
-        
+        response = requests.post(url, params=params)
+        response_data = response.json()
+
         s3_image_key_download = f'predictions/picture.jpg'
         original_img_path = f'/tmp/image.jpg'  # Temporary storage for downloaded image
         try:
-            # Download predicted image from S3
+            # Download the file from S3
             s3.download_file(bucket_name, s3_image_key_download, original_img_path)
-            logger.info(f'Downloaded prediction image completed from {bucket_name}/{s3_image_key_download}')
-            # send photo results to the Telegram end-user
-            self.send_photo(chat_id, original_img_path)
-        except FileNotFoundError:
-            logger.error("The file was not found.")
-            return "Predicted image not found", 404
-        except NoCredentialsError:
-            logger.error("AWS credentials not available.")
-            return "AWS credentials not available", 403
+            logger.info(f'Downloaded prediction image completed')
         except Exception as e:
-            logger.error(f"Error downloading file: {e}")
-            return f"Error downloading file: {e}", 500
+            logger.error(f'Error downloading {s3_image_key_download}: {e}')
+        # send photo results to the Telegram end-user
+        self.send_photo(chat_id, original_img_path)
 
-    
-    def handle_message(self, msg):
-        """Bot Main message handler"""
-        logger.info(f'Incoming message: {msg}')
-        if 'text' in msg:
-            self.send_text(msg['chat']['id'], f'Your original message: {msg["text"]}')
-        elif self.is_current_msg_photo(msg):
-            self.handle_photo_message(msg)
-        else:
-            self.send_text(msg['chat']['id'], "Unsupported message type")
-        
+        # Format the detected objects
+        labels = response_data.get('labels', [])
+        label_counts = {}
+        for label in labels:
+            label_class = label['class']
+            if label_class in label_counts:
+                label_counts[label_class] += 1
+            else:
+                label_counts[label_class] = 1
+
+        formatted_result = '\n'.join([f'{label}: {count}' for label, count in label_counts.items()])
+
+        # send the returned results to the Telegram end-user
+        self.send_text(chat_id, f'Detected objects:\n {formatted_result}')
+
 
 class ObjectDetectionBot(Bot):
     def handle_message(self, msg):
@@ -132,7 +132,3 @@ class ObjectDetectionBot(Bot):
 
         if self.is_current_msg_photo(msg):
             self.handle_photo_message(msg)
-
-            
-            # TODO send a job to the SQS queue
-            # TODO send message to the Telegram end-user (e.g. Your image is being processed. Please wait...)
