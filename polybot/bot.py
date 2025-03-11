7b5295e753a61ea578a463796ce9f585a7798674
ofekhalabi <Halabi1205@gmail.com>
2025-03-11 17:22:21 +0200

    update yolo5 code

--- polybot/bot.py
+++ polybot/bot.py
@@ -142,5 +142,5 @@ class ObjectDetectionBot(Bot):
         if self.is_current_msg_photo(msg):
             self.handle_photo_message(msg)
         else:
-            self.send_text(msg['chat']['id'], "this is your message: {msg['text']}")
+            self.send_text(msg['chat']['id'], f"this is your message: {msg['text']}")
 
